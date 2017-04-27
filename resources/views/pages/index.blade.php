e6850abe5f298126ed8b948fdb88ea03e24b8a00
Attila Kovacs <attila.kovacs92@gmail.com>
2017-04-27 21:24:43 +0200

    Setup Sass and Cretae PagesController

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1>M5 Truck Center</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,5 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', ['as'	=>	'pages.index', 'uses'	=> 'PagesController@home']);
+//Auth
 Auth::routes();
-
 Route::get('/home', 'HomeController@index');
