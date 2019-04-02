bd03f5a3a0d5d49e294e1f91a68d0ad73e07ad37
Tony <attila.kovacs92@gmail.com>
2019-04-02 14:28:20 +0200

    Change phone color

--- resources/views/partials/home/slider.blade.php
+++ resources/views/partials/home/slider.blade.php
@@ -6,7 +6,7 @@
                     <h1 class="c-header c-header__title">Non-stop Kamion és pótkocsi szerviz, azonnali indulással</h1>
                     <div>
                         <p class="c-header c-header__text" style="margin-bottom: 20px;">Rövid időn belül, az út mellett megjavítjuk!</p>
-                        <p class="c-header c-header__text">Hívjon a <a href="tel:+36308504403">+36308504403</a>-as számon!</p>
+                        <p class="c-header c-header__text">Hívjon a <a href="tel:+36308504403" class="c-header__phone">+36308504403</a>-as számon!</p>
                     </div>
                     <p class="c-header c-header__comment" style="margin: 20px 0;">M5 Truck Center kamion és pótkocsi szerviz, alkatrész kis- és nagykereskedelem.</p>
                     <a href="{{ route('pages.contact') }}" class="btn btn-contrast form-contact-submit text-center c-header__button">Ajánlatkérés</a>
